b2da6a15a9b16083f3a7f990755467cf6a9bf818
Zerio <neoboij@gmail.com>
2024-03-25 17:46:16 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -109,7 +109,7 @@ fn main() {
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    println!("{:?}", ARGS);
+    println!("{:?}", ARGS.to_vec());
 
     for stream in listener.incoming() {
         match stream {
